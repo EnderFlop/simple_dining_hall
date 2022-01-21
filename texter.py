64bedb4a2277840eebed5d3a6d84f0a58b2f3dc0
EnderFlop <enderflop@gmail.com>
2022-01-21 00:55:52 -0600

    Initial Commit
    simple program to text me the menu at Burge's international section everyday. Maybe I'll upgrade it one day. I tried to put it on the smart mirror but there's been a few bumps in the road, I'll try again soon.

---
+++ texter.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+
+#env variables
+load_dotenv()
+ACCOUNT_SID = os.environ.get("ACCOUNT_SID")
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
+PHONE_NUMBER = os.environ.get("PHONE_NUMBER")
+TWILIO_NUMBER = os.environ.get("TWILIO_NUMBER")
+
+#get info
+burge = requests.get("https://dining.uiowa.edu/burge-market-place")
+soup = BeautifulSoup(burge.text, features="html.parser")
+international_burge = soup.find(class_="panel panel-default marketplace-station marketplace-station-Burge-International")
+foods = international_burge.find_all(class_="h6 menu-item-title")
+
+#format string
+string = "Today's Menu:\n"
+for f in foods:
+  string += f.text + "\n"
+
+#send text
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+client.messages.create(to=PHONE_NUMBER, from_=TWILIO_NUMBER, body=string)
